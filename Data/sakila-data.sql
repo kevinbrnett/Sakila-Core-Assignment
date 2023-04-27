3bd7149c154b44e77ecc15bba05e381f7faed2ee
Kevin Barnett <kevin.brnett@gmail.com>
2023-04-27 10:16:32 -0600

    Answered the rest of the questions

--- Data/sakila-data.sql
+++ Data/sakila-data.sql
@@ -1,5 +1,8 @@
-select film.title, film.description, film.release_year, film.rating, film.special_features
+select film_category.film_id, film_category.category_id, film.title, film.description, film.release_year, film.rating, film.special_features, category.category_id
 from film_category
-join category on category.category_id = film_category.category_id
-join film on film.film_id = film_category.film_id
-where category.category_id = 'comedy';
+join category
+on category.category_id = film_category.category_id
+join film
+on  film.film_id = film_category.film_id
+where category.category_id = 'Comedy';
+
